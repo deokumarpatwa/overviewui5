3cb7d06c1baa6b08aabe1cd92e3e42513541488b
Deo Kumar <deokumarpatwa@gmail.com>
2024-08-11 11:49:30 +0000

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/ovp/app/Component"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("bkl.dk.zbkldktraveloverviewpage.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
